bb5d55db69a944050ec8b8189b3ac7504abce524
Piotr Hałas <Piotr.Halas@komputronik.pl>
2017-06-05 09:40:15 +0200

    fix example code

--- examples/custom_reject_handler.php
+++ examples/custom_reject_handler.php
@@ -2,14 +2,20 @@
 include_once "../vendor/autoload.php";
 
 $text = '<esi:include src="http://crazy-goat.com/octophpus/test/error_404"/>';
-
+/**
+ * @param string $data
+ * @param \CrazyGoat\Octophpus\EsiRequest[] $esiRequests
+ * @return Closure
+ */
 $reject_closure = function (string &$data, array $esiRequests) {
     return (function (\Exception $reason, int $index) use (&$data, $esiRequests) {
-        var_dump($reason->getCode());
+        echo "Unable to fetch request (".$esiRequests[$index]->getSrc().") reason : ".$reason->getMessage();
+        $reason;
     });
 };
 
 $mantle = new \CrazyGoat\Octophpus\Mantle(
-    ['on_reject' => $reject_closure ]
+    ['on_reject' => $reject_closure]
 );
+
 echo $mantle->decorate($text);
