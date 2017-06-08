00d05bc8bf9b8fc174f59a03c0d8064912535ce2
Piotr Hałas <Piotr.Halas@komputronik.pl>
2017-06-08 20:48:59 +0200

    add simple test

---
+++ test/EsiParserTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace CrazyGoat\Octophpus\Test;
+
+use CrazyGoat\Octophpus\EsiParser;
+use PHPUnit\Framework\TestCase;
+
+class EsiParserTest extends TestCase
+{
+
+    public function testEsiInclude()
+    {
+        $esiParser = new EsiParser();
+        self::assertEquals($esiParser->parse('test<esi:include />dasdas'), 1);
+        self::assertEquals($esiParser->parse('test<eSi:Include />dasdas'), 1);
+    }
+}
